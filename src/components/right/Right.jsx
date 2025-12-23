f5bb1941d6889c90bffc4d88cb3801435940241b
Sandy Nguyen <sandynguyen@MacBookPro.lan>
2025-12-23 15:21:27 -0800

    fr

--- src/components/right/Right.jsx
+++ src/components/right/Right.jsx
@@ -5,7 +5,7 @@ import "./right.scss";
 export default function Right() {
   return (
     <div className="right">
-        <img src="images/right-image.png" alt="Image" className="image image-mobile"/>
+        <img src="images/right-image-new.png" alt="Image" className="image image-mobile"/>
     </div>
   )
 }
